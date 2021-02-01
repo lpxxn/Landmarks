f71429255ad6b482aa1b3e1971da4a5feae4887f
lipeng <lipeng@meican.com>
2021-02-01 09:45:25 +0800

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by li on 2021/2/1.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
