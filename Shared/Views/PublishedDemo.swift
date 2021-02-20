4763e320bc07069d77e9443a575b3524bf924830
lpxxn <mi_duo@live.com>
2021-02-20 13:11:35 +0800

    test

---
+++ Shared/Views/PublishedDemo.swift
@@ -0,0 +1,47 @@
+//
+//  PublishedDemo.swift
+//  Landmarks
+//
+//  Created by li on 2021/2/20.
+//
+
+import SwiftUI
+
+struct PublishedDemo: View {
+    @ObservedObject var viewModelT = DataViewModelT1()
+    
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("count: \(viewModelT.value)")
+        }
+    }
+}
+
+class DataViewModelT1: ObservableObject {
+    @Published var value: Int = 0
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
+}
+
+/*
+ @Published是SwiftUI最有用的包装之一，允许我们创建出能够被自动观察的对象属性，SwiftUI会自动监视这个属性，一旦发生了改变，会自动修改与该属性绑定的界面。
+ 
+ @ObservedObject告诉SwiftUI，这个对象是可以被观察的，里面含有被@Published包装了的属性。
+ @ObservedObject包装的对象，必须遵循ObservableObject协议。也就是说必须是class对象，不能是struct。
+ @ObservedObject允许外部进行访问和修改。
+
+ */
+
+struct PublishedDemo_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            PublishedDemo()
+        }
+    }
+}
